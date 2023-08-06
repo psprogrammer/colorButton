ceefeab0a1a69e8f709ae3b0794487266cd643fd
Pooja Singh <poojasingh@Somdatts-MacBook-Air.local>
2023-08-06 18:56:02 +0530

    adding color changes with jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+//  import { logRoles } from '@testing-library/dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color, and updates when clicked ', () => {
+  //  const {container} = 
+  render(<App></App>);
+  //  logRoles(container);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' }); // name is options and text of the button is CTB
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
 });
+
+
+test('button turns blue when clicked ', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  expect(colorButton).toHaveTextContent('Change to red');
+}); // its empty so no errors thrown
+
+
+// we write our test before the actual funtionality 
